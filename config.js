16fb70d2af52207d558c0cbbfffb0babb98cfb0d
DARKDRAGONMD <143155935+DARKDRAGONMD@users.noreply.github.com>
2023-08-26 09:08:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741366868', 'me', true],
+  ['94741366868', 'SL THARUSHKA MAX', true], 
   [''] 
 ] //Number of owners
 
